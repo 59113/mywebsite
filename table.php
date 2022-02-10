832054c96254fe5de880a52feaa573504abf3a90
59113 <97937628+59113@users.noreply.github.com>
2022-02-10 21:59:33 +0700

    Update table.php

--- table.php
+++ table.php
@@ -1,19 +1,56 @@
 <html>
-  <head>
-    <title>ตารางของฉัน</title>
-  </head>
-  <body>
-    <table border="1">
-      <tr>
-        <th>ลำดับ</th>
-        <th>ชื่อ - นามสกุล</th>
-        <th>ชื่อเล่น</th>
-      </tr>
-      <tr>
-        <td>1</td>
-        <td>ยิ่งคุณ รอดทิม</td>
-        <td>ขม</td>
-      </tr>  
-    </table>
-  </body> 
+        <head>
+                <title>งานที่ 3</title>
+        </head>
+                <table border="1">
+                        <tr>
+                                <th>ลำดับ</th>
+                                <th>ชื่อ</th>
+                                <th>ชื่อเล่น</th>
+                                <th>รูป</th>
+                                <th>twitter</th>
+                        </tr>
+                        <tr>
+                                <td>1</td>
+                                <td>จอง แจฮยอน</td>
+                                <td>แจฮยอน</td>
+                                <td><img src="https://share.icloud.com/photos/091n2VZ1HkXBn2cvgwnlja50A" width="130px"></td>
+                                <td><a href="https://twitter.com/nctsmtown?s=21">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>2</td>
+                                <td>    </td>
+                                <td>   </td>
+                                <td><img src="" width="140px"></td>
+                                <td><a href="https://twitter.com/nctsmtown?s=21">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>3</td>
+                                <td>    </td>
+                                <td>   </td>
+                                <td><img src=" " width="140px"></td>
+                                <td><a href="https://twitter.com/nctsmtown?s=21">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>4</td>
+                                <td>  </td>
+                                <td>   </td>
+                                <td><img src=" " width="130px"></td>
+                                <td><a href="https://twitter.com/nctsmtown?s=21">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>5</td>
+                                <td>  </td>
+                                <td>  </td>
+                                <td><img src="   " width="130px"></td>
+                                <td><a href="https://twitter.com/nctsmtown?s=21">twitter</a></td>
+                        </tr>
+                        <tr>
+                                <td>6</td>
+                                <td>    </td>
+                                <td>  </td>
+                                <td><img src="" width="130px"></td>
+                                <td><a href="https://twitter.com/nctsmtown?s=21">twitter</a></td>
+                        </tr>
+        </body>
 </html>
