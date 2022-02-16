3c84bde8902696fc6d62629c351e994c4e0e4ae6
59113 <97937628+59113@users.noreply.github.com>
2022-02-16 23:35:52 +0700

    Create page2.php

---
+++ page2.php
@@ -0,0 +1,24 @@
+<html>
+	<head>
+		<title>การตกแต่งเว็บไซต์ด้วย CSS</title>
+		<style>
+			#box1{
+				width: 800px;
+				height: 350px;
+			}
+			.box{
+				background-color: yellow;
+			}
+			body{
+                                background-color: lightpink;
+			}
+			h1{
+				color: green;      
+		</style>
+	</head>
+	<body>
+		<div id="box1" class="box">
+			<h1>นางสาวกชกร นามสิมมา ชั้น 4/13 เลขที่ 13</h1>
+		</div>
+	</body>
+</html>
