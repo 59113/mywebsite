c59b8860393af6b60d897f449848a1d0f7019330
59113 <97937628+59113@users.noreply.github.com>
2022-02-28 23:59:43 +0700

    Create font.php

---
+++ font.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>กำหนดรูปแบบตัวอักษร</title>
+  <style>
+    body{
+      background-color: #FAEBD7;
+    }
+    @font-face{
+      font-family: myFont1;
+      src: url(MiPancake.woff)
+    }
+     @font-face{
+      font-family: myFont2;
+      src: url(iannnnn-DOG-light.woff)
+    }
+    #font1{
+      font-family: myFont1;
+    }
+    #font2{
+      font-family: myFont2;
+    }
+  </style>
+</head>
+<body>
+  <h1 id="font1">Pawarisa Jaiwong M.4/13 29 59113</h1>
+  <h1 id="font2">Pawarisa Jaiwong M.4/13 29 59113</h1>
+</body>
+</html>
